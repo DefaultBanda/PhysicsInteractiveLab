21a4afbc55e656f92ef83f9e6d25c4bfe3cfe965
Sengh <98918477+DefaultBanda@users.noreply.github.com>
2025-04-17 17:07:22 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: 'export', // ✅ Add export mode here
+}
+
+export default nextConfig;
